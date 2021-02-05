1df06d54aa63dfa58848fe3a56961b0080221e4a
Sam Ardis <ardis.productions@gmail.com>
2021-02-04 23:31:00 -0500

    Added navigation and dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import React from 'react';
 import './App.css';
+import { Home, About, Contact, Navigation } from './components';
+import { Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Hi, This is Ashley's Portfolio.
-      </h1>
+      <div>
+        <Navigation />
+        <Route exact path='/' component={Home} />
+        <Route path='/About' component={About} />
+        <Route path='/Contact' component={Contact} />
+      </div>
     </div>
   );
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <div>
+      <div className="navigationDiv">
+        <h1>Ashley Lee</h1>
+        <div>
+          <Link style={{textDecoration: "none", color: "white", fontSize: "1.5rem", fontWeight: "bold"}} to="/">Home</Link>
+        </div>
+        <div>
+          <Link style={{textDecoration: "none", color: "white", fontSize: "1.5rem", fontWeight: "bold"}} to="/About">About</Link>
+        </div>
+        <div>
+          <Link style={{textDecoration: "none", color: "white", fontSize: "1.5rem", fontWeight: "bold"}} to="/Contact">Contact</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
